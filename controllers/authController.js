fa3733ad882dc4868b4c60821392e4af6b418223
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2021-03-18 11:22:31 +0200

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,9 @@ const passport = require('passport');
 
 const login = (req, res) => {
   // TODO: add passport authenticate
+  passport.authenticate('local', {session: false}, (err, user, info) => {
+    console.log('login info', info);
+  });
 };
 
 module.exports = {
