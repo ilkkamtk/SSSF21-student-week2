6c8bead811e153bc5b683de95b5b4022ad115887
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2021-03-18 10:11:42 +0200

    update

--- routes/catRoute.js
+++ routes/catRoute.js
@@ -8,11 +8,14 @@ const {
 } = require('../controllers/catController');
 const router = express.Router();
 
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
+
 router.get('/', cat_list_get);
 
 router.get('/:id', cat_get);
 
-router.post('/', cat_create_post);
+router.post('/', upload.single('cat'), cat_create_post);
 
 router.put('/', (req, res) => {
   res.send('From this endpoint you can modify cats.');
